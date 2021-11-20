b0693a52407b52e56db57662aca63b1e85415345
Hamilton Evans <hamevans@Hamiltons-MBP.lan>
2021-11-19 22:14:02 -0800

    Mintable

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,15 @@ import './Home.css'
 import { Link } from 'react-router-dom'
 
 export default function Home () { 
-    /*<Link to="/mint">
-                        <button>MINT A VOYAGER NOW</button>
-                    </Link>
-    */
     return (
         <div className="home" id="#home">
             <div className="home__container">
                 <div className="home__wrapper">
                     <h1>GOLDEN VOYAGER PARTY</h1>
                     <h4>AN INCUBATOR AND ACCESS PASS FOR MUSICIANS, WRITERS, & CREATIVES</h4>
-                    <button>MINTING SOON</button>
+                    <Link to="/mint">
+                        <button>MINT A VOYAGER NOW</button>
+                    </Link>
                 </div>
             </div>
         </div>
